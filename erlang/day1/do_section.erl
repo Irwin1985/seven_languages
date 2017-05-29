03075f01f41153d319d90eae746e81b3db87d646
Robert Metcalf <rmetcalf9@googlemail.com>
2017-05-29 13:26:04 +0100

    Completed day 1

--- erlang/day1/do_section.erl
+++ erlang/day1/do_section.erl
@@ -1,6 +1,10 @@
 -module(do_section).
 -export([run/0]).
 
+%-- Command to run
+%--  c(do_section).
+%--  do_section:run().
+
 run() -> 
 	io:fwrite("Recursive function to return the number of words in a string!\n"),
 	run_num_words("The cat sat on the mat"),
@@ -12,13 +16,14 @@ run() ->
 	run_num_words("Really, is"),
 	run_num_words("Is a comma on it's own , a word?"),
 	run_num_words(" ab "),
+	io:fwrite("\n\nNow counting to 10 recursibly\n"),
+	count(1),
+	io:fwrite("\n\nPrint Message\n"),
+	print_message( {error,"There was an error"} ),
+	print_message( success ),
+	print_message( {success} ),
 	io:fwrite("End of run\n").
 
-run_num_words(Str) ->
-	io:fwrite(Str),
-	io:fwrite(": "),
-	io:fwrite(integer_to_list(num_words(Str))),
-	io:fwrite("\n").
 
 num_words(Str) ->
 	length(collect_words(Str,[],[])).
@@ -63,3 +68,27 @@ collect_words( Text, Words, Word) ->
 		end
 .		
 
+count(10) ->
+	io:fwrite(" 10\n");
+count(N) ->
+	io:fwrite(" "),
+	io:fwrite(integer_to_list(N)),
+	io:fwrite("\n"),
+	count(N+1).
+
+run_num_words(Str) ->
+	io:fwrite(Str),
+	io:fwrite(": "),
+	io:fwrite(integer_to_list(num_words(Str))),
+	io:fwrite("\n").
+
+print_message( Value ) ->
+	case Value of 
+		success ->
+			io:format( "success~n" );
+		{success} ->
+			io:format( "success~n" );
+		{error, Message} ->
+			io:format( "error: " ++ Message ++ "~n" )
+	end
+.
