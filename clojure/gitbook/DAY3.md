5736d8a309bf6064120b0fe342e74203373def0f
Robert Metcalf <rmetcalf9@googlemail.com>
2017-06-28 20:05:42 +0100

    Clojure day 3

--- clojure/gitbook/DAY3.md
+++ clojure/gitbook/DAY3.md
@@ -1,3 +1,27 @@
 # Seven Languages Day 3
 
+Lots of use of Java here so not in the gitbook
 
+## Find a queue implementation that blocks when the queue is empty and  waits for a new item in the queue
+
+seque
+
+http://clojuredocs.org/clojure.core/seque
+
+```eval-clojure
+(let [start (System/nanoTime)
+             q (seque
+                 (iterate
+                   #(do (Thread/sleep 400) (inc %))
+                   0))]
+         (println "sleep five seconds...")
+         (Thread/sleep 5000)
+         (doseq [i (take 20 q)]
+           (println (int (/ (- (System/nanoTime) start) 1e7))
+                    ":" i)))
+```
+
+## Do: Use refs to create a vector of accounts in memory. Create debit and credit functions to charge the balance of an account
+
+Didn't do myself but looked at:
+https://www.bennadel.com/blog/2101-seven-languages-in-seven-weeks-clojure---day-3.htm
--- clojure/project/src/project/core.clj
+++ clojure/project/src/project/core.clj
@@ -57,9 +57,157 @@
      (println "End of Day2")
   )  
 )
+
+(defn transfer-money [a1 a2 amount]
+  (dosync
+    (alter a1 - amount)
+    (alter a2 + amount)
+    amount)
+)
+
+(defn credit [acc amount]
+  (dosync
+    (alter acc + amount)
+    amount)
+)
+
+(defn debit [acc amount]
+  (credit acc (* -1 amount))
+)
+
+;barber shop problem
+
+(def open-for-business? (atom false))
+(def haircut-count (atom 0))
+(def waiting-room (ref []))
+(def waiting-room-size 3)
+
+(defn
+    open-shop
+    [duration]
+    (future 
+      (println "Opening Shop")
+      (reset! open-for-business? true)
+      (Thread/sleep duration)
+      (println "Closing Shop")
+      (reset! open-for-business? false)
+    )
+)
+
+(defn
+    display
+    []
+    (do 
+      (print "\033[u")
+      (print "Cuts: " @haircut-count)
+      (print " Wait Room: " (dosync (count (ensure waiting-room))))
+    )
+)
+
+(defn
+    add-customers
+    []
+    (future
+      (do
+        (println "Starting to add customers")
+        (while
+            @open-for-business?
+            (dosync
+                (if
+                    (<
+                        (count (ensure waiting-room))
+                        waiting-room-size
+                    )
+                    ;-- The waiting room is now full
+                    (alter waiting-room conj :customer)
+                )
+            )
+            (Thread/sleep
+                (+ 10 (rand-int 71))
+            )
+        )
+        (println "Finished adding customers")
+      )
+    )
+)
+(defn
+    cut-hair
+    []
+    (future
+      (do
+        (println "Starting to cut-hair")
+        (while
+            (or @open-for-business? (dosync (count (ensure waiting-room))))
+            (when-let
+                [
+                    next-customer
+                    (dosync
+                        (let
+                            [
+                                next-customer
+                                (first (ensure waiting-room))
+                            ]
+                            ;-- We found the next customer
+                            (when
+                                next-customer
+                                ;-- take the first customer off the list
+                                (alter waiting-room rest)
+                                next-customer
+                            )
+                        )
+                    )
+                ]
+                (Thread/sleep 80)
+                (swap! haircut-count inc)
+            )
+        )
+        (println "Finished cutting hair")
+      )
+    )
+)
+
 (defn day3 [] 
   (do
      (println "\n\nStart of Day3")
+
+     (println "Create bank account acc1 with 100 in it")
+     (def acc1 (ref 100))
+     (println "Create bank account acc2 with 100 in it")
+     (def acc2 (ref 200))
+
+     (println "Acc 1 Balance=" @acc1)
+     (println "Acc 2 Balance=" @acc2)
+
+     (println "Moving 20 from acc1 to acc2")
+     (transfer-money acc1 acc2 20)
+
+     (println "Acc 1 Balance=" @acc1)
+     (println "Acc 2 Balance=" @acc2)
+
+     (println "\n\nUsing credit and debit - breaks my double entry account system but the book told me to do it")
+     (credit acc1 19000)
+     (debit acc2 19001)
+
+     (println "Acc 1 Balance=" @acc1)
+     (println "Acc 2 Balance=" @acc2)
+
+     (println "\n\n\n-------------------\nOk now the barber problem\n------------")
+
+     (open-shop (* 10 1000))
+     ;--(open-shop (* 10 50))
+     (cut-hair)
+     (add-customers)
+
+     (print "\033[s")
+     (while
+          @open-for-business?
+          (display)
+     )
+     (println "")
+     (Thread/sleep 100)
+     (println "Number of cuts:" @haircut-count)
+
+
      (println "End of Day3")
   )  
 )
